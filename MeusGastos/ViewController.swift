3beb3c779e8b6958c2740aa2a500a8a7bafff900
Alan Pitanga <alansantosbahia@gmail.com>
2023-11-15 20:06:06 -0300

    Create Base Project

---
+++ MeusGastos/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MeusGastos
+//
+//  Created by Alan Pitanga on 15/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+}
