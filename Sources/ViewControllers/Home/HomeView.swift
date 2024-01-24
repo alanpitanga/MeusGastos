c7200ad5a25b0bde196d4ce4634e2c92b7618227
Alan Pitanga <alansantosbahia@gmail.com>
2024-01-23 22:00:53 -0300

    add setViewInputs and setViewOutputs

--- Sources/ViewControllers/Home/HomeView.swift
+++ Sources/ViewControllers/Home/HomeView.swift
@@ -10,12 +10,28 @@ import UIKit
 
 final class HomeView: UIView {
     
+    let width: CGFloat = UIScreen.main.bounds.width - 36
+    
     lazy var segmentedControl: UISegmentedControl = {
         let segment = UISegmentedControl(items: ["Essa semana", "Esse mês", "Todos"])
         segment.translatesAutoresizingMaskIntoConstraints = false
         segment.selectedSegmentIndex = 1
         segment.backgroundColor = .lightGray
         return segment
+    }()
+    
+    lazy var viewInputs: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    lazy var viewOutputs: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .green
+        return view
     }()
     
     override init(frame: CGRect) {
@@ -29,6 +45,8 @@ final class HomeView: UIView {
     
     private func setElementVisual() {
         setSegmented()
+        setViewInputs()
+        setViewOutputs()
     }
     
     private func setSegmented() {
@@ -38,6 +56,28 @@ final class HomeView: UIView {
             segmentedControl.topAnchor.constraint(equalTo: self.topAnchor, constant: 70),
             segmentedControl.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12),
             segmentedControl.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -12)
+        ])
+    }
+    
+    private func setViewInputs() {
+        self.addSubview(viewInputs)
+        
+        NSLayoutConstraint.activate([
+            viewInputs.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 16),
+            viewInputs.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12),
+            viewInputs.widthAnchor.constraint(equalToConstant: width / 2),
+            viewInputs.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    
+    private func setViewOutputs() {
+        self.addSubview(viewOutputs)
+        
+        NSLayoutConstraint.activate([
+            viewOutputs.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 16),
+            viewOutputs.leftAnchor.constraint(equalTo: viewInputs.rightAnchor, constant: 12),
+            viewOutputs.widthAnchor.constraint(equalToConstant: width / 2),
+            viewOutputs.heightAnchor.constraint(equalToConstant: 150)
         ])
     }
 }
